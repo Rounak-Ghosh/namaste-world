b08d4c8f86bb0349ce89d8fc6bbf0348277b2f16
Rounak-Ghosh <rounakghosh189@gmail.com>
2022-06-19 23:00:31 +0530

    Header and Footer Component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,11 @@
 import Menu from './MenuComponent';
+import Home from './HomeComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
 import { Component } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
 
@@ -16,18 +18,22 @@ class Main extends Component {
     };
   }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId });
+  const HomePage = () => {
+    return(
+        <Home 
+        />
+    );
   }
 
   render() {
     return (
       <div>
         <Header />
-        <Menu dishes={this.state.dishes}
-            onClick={(dishId) => this.onDishSelect(dishId)}/>
-        <DishDetail 
-            dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        <Switch>
+          <Route path='/home' component={HomePage} />
+          <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+          <Redirect to='/home' />
+        </Switch>
         <Footer />
       </div>
     );
